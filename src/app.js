259df1dae9f6e34e1181ff7c14336c7f579012a6
PedroParraPerez <pedroparra06091994@gmail.com>
2022-01-21 11:13:19 +0000

    conditionals

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import "./style.css";
-
 window.onload = function() {};
 
+const OnlyLetters = /^[a-z][a-z\s]*$/;
+const form = document.querySelector(".myForm");
 const alert = document.querySelector("#alert");
 const name = document.querySelector("#name");
 const surname = document.querySelector("#surname");
@@ -9,13 +10,43 @@ const age = document.querySelector("#age");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const repPass = document.querySelector("#repPass");
+const submit = document.querySelector("#submit");
+const inputs = document.querySelectorAll(".inputs");
 
-name.addEventListener("submit", e => {
-  e.preventDefault();
-  console.log(name);
-  if (name.value.length >= 5) {
-    console.log("es correcto");
+name.addEventListener("focusout", e => {
+  if (name.value.match(OnlyLetters)) {
+    name.classList.remove("isinvalid");
+    name.classList.add("isvalid");
   } else {
-    console.log("no es correcto");
+    name.classList.remove("isvalid");
+    name.classList.add("isinvalid");
+  }
+});
+surname.addEventListener("focusout", e => {
+  if (surname.value.match(OnlyLetters)) {
+    surname.classList.remove("isinvalid");
+    surname.classList.add("isvalid");
+  } else {
+    surname.classList.remove("isvalid");
+    surname.classList.add("isinvalid");
+  }
+});
+password.addEventListener("focusout", e => {
+  if (password.value.length > 7) {
+    password.classList.remove("isinvalid");
+    password.classList.add("isvalid");
+  } else {
+    password.classList.remove("isvalid");
+    password.classList.add("isinvalid");
+  }
+});
+
+repPass.addEventListener("focusout", e => {
+  if (repPass.value == password.value && password.value.length > 7) {
+    repPass.classList.remove("isinvalid");
+    repPass.classList.add("isvalid");
+  } else {
+    repPass.classList.remove("isvalid");
+    repPass.classList.add("isinvalid");
   }
 });
